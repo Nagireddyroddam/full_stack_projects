0afad5a30d7266c294ee87c0bc7f3d76fdd0f9f6
Nagireddy Roddam <roddamnagireddy1@gmail.com>
2026-05-16 12:48:37 +0530

    added delete feature to do list

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,6 +2,9 @@ import { useState } from 'react';
 function App() {
   const[task,setTask]=useState(["wake up","Study react"]);
   const [input, setInput] = useState('');
+  function deleteTask(index) {
+  setTask(task.filter((t, i) => i !== index));
+}
   return(
     <div>
       <h1>My Todo List</h1>
@@ -12,11 +15,15 @@ function App() {
 />
 
       <button onClick={() => { setTask([...task, input]); setInput(''); }}>Add Task</button>
-    <ul>
-      {task.map((task,index)=>(
-      <li key={index}>{task}</li>
-    ))}
-    </ul>
+  <ul>
+  {task.map((task, index) => (
+    <li key={index}>
+      {task}
+      <button onClick={() => deleteTask(index)}>Delete</button>
+    </li>
+  ))}
+
+  </ul>
 
     </div>
   )
