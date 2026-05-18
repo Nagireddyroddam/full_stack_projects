2cc3086d8d4856dffcaa5afe738223991726f158
Nagireddy Roddam <roddamnagireddy1@gmail.com>
2026-05-18 10:28:05 +0530

    added delete rout with postgreSQL

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,31 +1,59 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import './App.css';
+
 function App() {
-  const[task,setTask]=useState(["wake up","Study react"]);
+  const [task, setTask] = useState([]);
   const [input, setInput] = useState('');
+
   function deleteTask(index) {
-  setTask(task.filter((t, i) => i !== index));
-}
+    setTask(task.filter((t, i) => i !== index));
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:5000/tasks')
+      .then(res => res.json())
+      .then(data => setTask(data));
+  }, []);
+  function addTask(){
+    fetch('http://localhost:5000/tasks', {
+      method:'post',
+      headers:{'Content-Type': 'application/json'},
+      body: JSON.stringify({task: input})
+    })
+    .then(res => res.json())
+    .then(data =>{
+      setTask(data);
+      setInput(''); 
+
+    });
+  }
+  function deleteTask(id){
+    fetch(`http://localhost:5000/tasks/${id}`, {
+      method:'DELETE',
+    })
+    .then(res => res.json())
+    .then(data => setTask(data));
+  }
+
   return(
     <div>
       <h1>My Todo List</h1>
-      <input 
-        value={input} 
-        onChange={(e) => setInput(e.target.value)} 
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         placeholder="Add a task"
-/>
-
-      <button onClick={() => { setTask([...task, input]); setInput(''); }}>Add Task</button>
-  <ul>
-  {task.map((task, index) => (
-    <li key={index}>
-      {task}
-      <button onClick={() => deleteTask(index)}>Delete</button>
-    </li>
-  ))}
-
-  </ul>
-
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {task.map((task, index) => (
+          <li key={index}>
+            {task.task}
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
+
 export default App;
